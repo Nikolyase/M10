cb5d7f8eb566065992e7ba0343a94932e846c0c0
Nikolyase <96522347+Nikolyase@users.noreply.github.com>
2022-03-09 21:39:11 +0300

    Добавьте файлы проекта.

---
+++ M10/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace M10
+{
+    public class Calc : ICalculator, IException
+    {
+        ILogger Logger { get; }
+        public Calc (ILogger logger)
+        {
+            Logger = logger;
+        }
+        
+        public void Solve(int numberOne, int numberTwo)
+        {
+            Logger.Event($"Сумма чисел {numberOne} и {numberTwo} равна {numberOne + numberTwo}");
+        }
+        
+        public void ExpErr()
+        {
+            Logger.Error("Введено некорректное значение, вводите числа!");
+        }
+        
+        public void FinColor()
+        {
+            Logger.BaseColor();
+        }
+    }
+}
+
---
+++ M10/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace M10
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            var calc = new Calc (Logger);
+            Console.WriteLine("Калькулятор сложения 2-х чисел.");
+            while (true)
+            {
+                Console.WriteLine();
+                try
+                {
+                    Console.WriteLine("Введите первое число:");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите второе число:");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    calc.Solve(number1, number2);
+                }
+                catch (FormatException)
+                {
+                    calc.ExpErr();
+                }
+                finally
+                {
+                    calc.FinColor();
+                }
+
+            }
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+        void BaseColor();
+    }
+    
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+        public void BaseColor()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+
+    public interface ICalculator
+    {
+        void Solve(int numberOne, int numberTwo);
+
+    }
+    
+    public interface IException
+    {
+        void ExpErr();
+        void FinColor();
+    }
+
+}
